5333be181a618e5937ba79a4c8b49c89027a948c
omarBsghaier <98103641+omarBsghaier@users.noreply.github.com>
2023-02-12 00:55:49 +0100

    Create Convert the Temperature.cpp

---
+++ Convert the Temperature.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<double> convertTemperature(double celsius) {
+        vector<double> v ;
+        v.push_back(celsius + 273.15) ;
+        v.push_back(celsius*1.8 + 32.00)  ;
+        return (v) ; 
+        
+    }
+};
